78a97814ff318252417c10d2d936d6031b13f90c
tt920030 <t920030@icloud.com>
2023-04-09 16:46:00 +0800

    註冊js

--- js/resigner.js
+++ js/resigner.js
@@ -1,71 +1,128 @@
-$(function(){
+$(function () {
+    
+    // 發送驗證碼
     $(".phone").click(function(e){
         e.preventDefault();
-    });
+        let sent_CAPTCHA = true;
+        let phoneNum = /^09[0-9]{8}$/;
+        let taiwan = phoneNum.test($("#phone").val());
 
-    $(".CAPTCHA").click(function(e){
-        e.preventDefault();
-    });
-
-    $(".resigner input[type='submit']").click(function(e){
-        let send_data = true;
-
-        if($("#name").val() === ""){
-            $("#name").addClass("-error");
-            send_data = false;
-        }else{
-            $("#name").removeClass("-error");
-        }
-
-        if($("#email").val() === ""){
-            $("#email").addClass("-error");
-            send_data = false;
-        }else{
-            $("#email").removeClass("-error");
-        }
-
-        if(is.email($("#email").val())){
-            $("#email").removeClass("-error");
-        }else{
-            send_data = false;
-            $("#email").addClass("-error");
-        }
-
-        if($("#phone").val() === ""){
+        if($("#phone").val() === "" || taiwan === false){
             $("#phone").addClass("-error");
-            send_data = false;
+            $("#phone").parent().next().addClass("-alert");
+            sent_CAPTCHA = false;
         }else{
             $("#phone").removeClass("-error");
+            $("#phone").parent().next().removeClass("-alert");
         }
 
-        if($("#CAPTCHA").val() === ""){
+        if (sent_CAPTCHA) {
+            alert("已傳送驗證碼");
+        }
+    });
+    
+
+    // 驗證碼確認
+    $(".CAPTCHA").click(function (e) {
+        e.preventDefault();
+        let correct = true;
+
+        if ($("#CAPTCHA").val() === "") {
             $("#CAPTCHA").addClass("-error");
-            send_data = false;
-        }else{
+            $("#CAPTCHA").parent().next().addClass("-alert");
+            correct = false;
+        } else {
             $("#CAPTCHA").removeClass("-error");
+            $("#CAPTCHA").parent().next().removeClass("-alert");
         }
 
-        if($("#password").val() === ""){
-            $("#password").addClass("-error");
+        if (correct) {
+            alert("驗證碼正確");
+        }
+    });
+
+    $(".resigner input[type='submit']").click(function (e) {
+        let send_data = true;
+
+        // 姓名不可空白
+        if ($("#name").val() === "") {
+            $("#name").addClass("-error");
+            $("#name").next().addClass("-alert");
             send_data = false;
-        }else{
+        } else {
+            $("#name").removeClass("-error");
+            $("#name").next().removeClass("-alert");
+        }
+
+        // email格式
+        // email不可空白
+        if ($("#email").val() === "" || is.email($("#email").val()) === false) {
+            $("#email").addClass("-error");
+            $("#email").next().addClass("-alert");
+            send_data = false;
+        } else {
+            $("#email").removeClass("-error");
+            $("#email").next().removeClass("-alert");
+        }
+
+        // 手機不可空白
+        let phoneNum = /^09[0-9]{8}$/;
+        let taiwan = phoneNum.test($("#phone").val());
+
+        if ($("#phone").val() === "" || taiwan === false) {
+            $("#phone").addClass("-error");
+            $("#phone").parent().next().addClass("-alert");
+            send_data = false;
+        } else {
+            $("#phone").removeClass("-error");
+            $("#phone").parent().next().removeClass("-alert");
+        }
+
+        // 驗證碼不可空白
+        if ($("#CAPTCHA").val() === "") {
+            $("#CAPTCHA").addClass("-error");
+            $("#CAPTCHA").parent().next().addClass("-alert");
+            send_data = false;
+        } else {
+            $("#CAPTCHA").removeClass("-error");
+            $("#CAPTCHA").parent().next().removeClass("-alert");
+        }
+
+        // 密碼格式 
+        // 密碼不可空白
+        let strongPass = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$/;
+        let ok = strongPass.test($("#password").val());
+
+        if (ok) {
             $("#password").removeClass("-error");
+            $("#password").next().removeClass("-alert");
+        } else if ((ok === false && $("#password").val().length < 6) || $("#password").val() === "") {
+            $("#password").addClass("-error");
+            $("#password").next().addClass("-alert");
+            send_data = false;
+        } else {
+            $("#password").addClass("-error");
+            $("#password").next().text("*密碼須包含大小寫英文字母與數字");
+            $("#password").next().addClass("-alert");
+            send_data = false;
         }
 
-        if($("#password-comfirm").val() === ""){
+        // 確認密碼不可空白
+        // 二次密碼需相符 
+        if ($("#password-comfirm").val() === "" || ($("#password").val() !== $("#password-comfirm").val())) {
             $("#password-comfirm").addClass("-error");
+            $("#password-comfirm").next().addClass("-alert");
             send_data = false;
-        }else{
+        } else {
             $("#password-comfirm").removeClass("-error");
-        }
-
-        if($("#password").val() !== $("#password-comfirm").val()){
-            $("#password-comfirm").addClass("-error");
-            send_data = false;
+            $("#password-comfirm").next().removeClass("-alert");
         }
 
         if (!send_data) {
             e.preventDefault();
+        } else {
+            alert("註冊成功!");
         }
     });
+
 });
